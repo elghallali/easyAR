7114d724918b5ab1dc783639ec8b9ab88e425ecb
SmartProjects <36716111+lotfi1002@users.noreply.github.com>
2020-01-26 22:10:25 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
+#halper libraries
 import argparse
 import numpy as np 
+#opencv library
 import cv2
+# pygame library and open cv screen
 from lib.camera import *
 from lib.pygamescene import *
+from lib.marker import *
+
 
 def main() : 
     print("=================The main Method of AR Application=================")
-    
+    tenser = MarkerDetection()
     #camera = Camera()
     #camera.steam()
     pg = ArPyGameScene()
